b324d62ead9331f296c201779c375bf4d6b44cec
yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasu <129604598+yasunari1103@users.noreply.github.com>
2024-09-24 01:55:57 +0900

    ジャンピングまーん

--- games/1_easy.js
+++ games/1_easy.js
@@ -178,8 +178,10 @@ game.onload = function startGame() {
     num.x = 420 - num.width / 2;
     num.y = 160 - num.height / 2;
     primeButton.image = surfacePrimeSelected;
+    sdIllust.y -= 10;
     setTimeout(() => {
       primeButton.image = surfacePrime;
+      sdIllust.y += 10;
     }, 100);
   });
   //Pキーが押されたら実行
@@ -208,8 +210,10 @@ game.onload = function startGame() {
         num.x = 420 - num.width / 2;
         num.y = 160 - num.height / 2;
         primeButton.image = surfacePrimeSelected;
+        sdIllust.y -= 10;
         setTimeout(() => {
           primeButton.image = surfacePrime;
+          sdIllust.y += 10;
         }, 100);
       }
     }
@@ -238,8 +242,10 @@ game.onload = function startGame() {
     num.x = 420 - num.width / 2;
     num.y = 160 - num.height / 2;
     compositeButton.image = surfaceCompositeSelected;
+    sdIllust.y -= 10;
     setTimeout(() => {
       compositeButton.image = surfaceComposite;
+      sdIllust.y += 10;
     }, 100);
   });
   //Cキーが押されたら実行
@@ -268,8 +274,10 @@ game.onload = function startGame() {
         num.x = 420 - num.width / 2;
         num.y = 160 - num.height / 2;
         compositeButton.image = surfaceCompositeSelected;
+        sdIllust.y -= 10;
         setTimeout(() => {
           compositeButton.image = surfaceComposite;
+          sdIllust.y += 10;
         }, 100);
       }
     }
