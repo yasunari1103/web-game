6a6df888507a9d15c9a3e8e3eb7adaecbba40e0b
yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasu <129604598+yasunari1103@users.noreply.github.com>
2024-09-28 03:39:18 +0900

    進捗だめです

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+var game;
 document.addEventListener("DOMContentLoaded", () => {
-  console.log(globalVars.maxNum);
   const target = document.body;
   //mutationobserverでDOMを監視
   const observer = new MutationObserver((mutations) => {
@@ -10,21 +10,15 @@ document.addEventListener("DOMContentLoaded", () => {
         let div = document.createElement("div");
         div.id = "resultScene";
         const homeButton = document.createElement("button");
-        const retryButton = document.createElement("button");
         const homeImage = document.createElement("img");
         homeImage.src = "ボタン素材/resultボタン/home.png";
-        const retryImage = document.createElement("img");
-        retryImage.src = "ボタン素材/resultボタン/retry.png";
         homeButton.appendChild(homeImage);
-        retryButton.appendChild(retryImage);
 
         // 追加前にボタンにIDを付与しておく
         homeButton.id = "homeButton";
-        retryButton.id = "retryButton";
 
         div.appendChild(resultImage);
         div.appendChild(homeButton);
-        div.appendChild(retryButton);
 
         let styleImage = document.createElement("style");
         styleImage.textContent =
@@ -42,25 +36,11 @@ document.addEventListener("DOMContentLoaded", () => {
         homeButton.style.left = "4%";
         homeButton.style.top = "20%";
         homeButton.style.border = "none";
-        retryButton.style.height = "8%";
-        retryButton.style.padding = "0";
-        retryButton.style.margin = "0";
-        retryButton.style.backgroundColor = "rgba(0, 0, 0, 0)";
-        retryButton.style.position = "absolute";
-        retryButton.style.left = "19%";
-        retryButton.style.top = "20%";
-        retryButton.style.border = "none";
         homeButton.onclick = function () {
           location.reload();
         };
-        retryButton.onclick = function () {
-          const game = new Game(625, 441);
-          game.onload = startGame;
-          game.start();
-        };
 
         document.body.appendChild(div);
-        console.log("a");
       }
     });
   });
